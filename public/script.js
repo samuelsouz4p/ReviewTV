a77d148afa930b80c226573e2a38ba94d952846a
Samuel Souza <samuelmsp07@gmail.com>
2025-03-15 17:59:10 -0300

    organizando pastas

--- public/script.js
+++ public/script.js
@@ -2,10 +2,10 @@ function menuShow() {
     let menuMobile = document.querySelector('.mobile-menu');
     if (menuMobile.classList.contains('open')) {
         menuMobile.classList.remove('open');
-        document.querySelector('.icon').src = "imagens/menu_white_36dp.svg";
+        document.querySelector('.icon').src = "/public/imagens/menu_white_36dp.svg";
     } else {
         menuMobile.classList.add('open');
-        document.querySelector('.icon').src = "imagens/close_white_36dp.svg";
+        document.querySelector('.icon').src = "/public/imagens/close_white_36dp.svg";
     }
 }
 
